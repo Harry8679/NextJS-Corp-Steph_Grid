a078b21d145724f0f4af14374d954130f163c8aa
Harry EMARH <emarh.harry.code@gmail.com>
2024-05-29 01:09:29 +0200

    More on the Image Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div>
       Home Page
-      <Image src={homeImg} alt='home image' fill style={{ objectFit: 'cover' }} />
+      {/* <Image src={homeImg} alt='home image' fill style={{ objectFit: 'cover' }} /> */}
+      <Image src={homeImg} alt='image' width={800} height={600} />
+      <div>Buy our product</div>
     </div>
   );
 }
