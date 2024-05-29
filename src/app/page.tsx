53e125715dc64cc004707aa7950a44f317986083
Harry EMARH <emarh.harry.code@gmail.com>
2024-05-29 02:53:21 +0200

    Adding Reusable Presentation Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 import homeImg from 'public/home.jpg';
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      {/* <Image src={homeImg} alt='home image' fill style={{ objectFit: 'cover' }} /> */}
-      <Image src={homeImg} alt='image' width={800} height={600} />
-      <div>Buy our product</div>
-    </div>
+    <Hero imgData={homeImg} imgAtl='car factory' title='Professional Cloud Hosting' />
   );
 }
